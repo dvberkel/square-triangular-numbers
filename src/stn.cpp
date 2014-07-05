32f83f127ea6b2e053ef915acf6c199ec571ff9e
dvberkel <daan.v.berkel.1980@gmail.com>
2014-07-05 08:56:43 +0200

    calculate the indices

--- src/stn.cpp
+++ src/stn.cpp
@@ -7,8 +7,24 @@ signed long long n(signed long long n0, signed long long n1) {
   return 34 * n1 - n0 + 2;
 }
 
+signed long long s0 = 0;
+signed long long s1 = 1;
+
+signed long long s(signed long long s0, signed long long s1) {
+  return 6 * s1 - s0;
+}
+
+signed long long t0 = 0;
+signed long long t1 = 1;
+
+signed long long t(signed long long t0, signed long long t1) {
+  return 6 * t1 - t0 + 2;
+}
+
 int main(){
-  signed long long result = n(n0, n1);
-  std::cout << result << std::endl;
+  signed long long nn = n(n0, n1);
+  signed long long sn = s(s0, s1);
+  signed long long tn = t(t0, t1);
+  std::cout << nn << "," << sn << "," << tn << std::endl;
   return 0;
 }
