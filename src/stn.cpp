a77265859858ca61cb54a0ff779ef6361e64cdae
dvberkel <daan.v.berkel.1980@gmail.com>
2014-07-05 08:52:00 +0200

    calculate the square triangle number

--- src/stn.cpp
+++ src/stn.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+signed long long n0 = 0;
+signed long long n1 = 1;
+
+signed long long n(signed long long n0, signed long long n1) {
+  return 34 * n1 - n0 + 2;
+}
+
 int main(){
-  std::cout << "Hello World" << std::endl;
+  signed long long result = n(n0, n1);
+  std::cout << result << std::endl;
   return 0;
 }
